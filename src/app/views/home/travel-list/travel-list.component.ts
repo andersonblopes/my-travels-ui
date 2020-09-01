b72628d8ffab19ee0ef010dd2c93c9fcceb62fdb
Anderson Lopes <anderson.lopes.2010@gmail.com>
2020-09-01 10:21:35 +0200

    includes api consumer.

--- src/app/views/home/travel-list/travel-list.component.ts
+++ src/app/views/home/travel-list/travel-list.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { TravelService } from 'src/app/shared/service/travel.service';
+import { Travel } from 'src/app/shared/model/travel.model';
+
+
 @Component({
   selector: 'app-travel-list',
   templateUrl: './travel-list.component.html',
@@ -7,9 +11,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TravelListComponent implements OnInit {
 
-  constructor() { }
+  travelList: Travel[];
+
+  constructor(
+    public travelService: TravelService
+  ) { }
 
   ngOnInit(): void {
+    this.getTravels();
+  }
+
+  getTravels(){
+    this.travelService.getTravels().subscribe(data => {
+      this.travelList = data.content;
+
+      console.log(this.travelList);
+    });
   }
 
 }
